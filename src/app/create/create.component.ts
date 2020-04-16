8443d3cffdc6df313e063b3836af9a429e8c806f
sansar90 <sansargarg42@gmail.com>
2020-04-16 13:05:51 +0530

    1 file modified

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 @Component({
   selector: 'app-create',
@@ -6,8 +9,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
-
- editorConfig:any;
+editorConfig:any;
+title:string;
+content:string;
 
   constructor() { 
     this.editorConfig=
@@ -34,7 +38,24 @@ export class CreateComponent implements OnInit {
   }
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
+  }
+
+  cratePost(){
+firebase.firestore().settings({
+  timestampsInSnapshots:true
+});
+
+firebase.firestore().collection("posts").add({
+  title:this.title,
+  content:this.content,
+  owner:firebase.auth().currentUser.uid,
+  created:firebase.firestore.FieldValue.serverTimestamp()
+}).then((data)=>{
+  console.log(data);
+}).catch((error)=>{
+  console.log(error);
+})
   }
 
 }
