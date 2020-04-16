955133d9927238dab1a6c4cd80b658caa1bf12d2
sansar90 <sansargarg42@gmail.com>
2020-04-16 12:30:40 +0530

    2 files modified

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -7,7 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+ editorConfig:any;
+
+  constructor() { 
+    this.editorConfig=
+    {
+      "editable": true,
+      "spellcheck": true,
+      "height": "auto",
+      "minHeight": "0",
+      "width": "auto",
+      "minWidth": "0",
+      "translate": "yes",
+      "enableToolbar": true,
+      "showToolbar": true,
+      "placeholder": "Enter text here...",
+      "imageEndPoint": "",
+      "toolbar": [
+          ["bold", "italic", "underline", "strikeThrough", "superscript", "subscript"],
+          ["fontName", "fontSize", "color"],
+          ["justifyLeft", "justifyCenter", "justifyRight", "justifyFull", "indent", "outdent"],
+          ["cut", "copy", "delete", "removeFormat", "undo", "redo"],
+          ["paragraph", "blockquote", "removeBlockquote", "horizontalLine", "orderedList", "unorderedList"],
+          ["link", "unlink", "image", "video"]
+      ]
+  }
+  }
 
   ngOnInit() {
   }
